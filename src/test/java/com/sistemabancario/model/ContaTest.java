f2687b630274ccbca7585efaf72e9c5355a3d833
rene <rene@10.0.0.15>
2020-12-22 21:39:59 -0300

    Implementação dos requisitos de R01 a R04 para a Classe Conta.

--- src/test/java/com/sistemabancario/model/ContaTest.java
+++ src/test/java/com/sistemabancario/model/ContaTest.java
@@ -5,10 +5,71 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ContaTest {
     
+//    @Test
+//    void testAddMovimentacao() {
+//        //TODO: Você precisa implementar este teste
+//    }
+    
     @Test
-    void testAddMovimentacao() {
-        //TODO: Você precisa implementar este teste
+    void testSetNumeroValido(){
+        final Conta instance = new Conta();
+        final String esperado = "12345-6";
+        instance.setNumero(esperado);
+        final String obtido = instance.getNumero();
+        assertEquals(esperado, obtido);
     }
     
+    @Test
+    void testSetNumeroInvalidoNaoArmazenado(){
+        final Conta instance = new Conta();
+        final String invalido = "123";
+        assertThrows(IllegalArgumentException.class, () -> instance.setNumero(invalido));
+        final String obtido = instance.getNumero();
+        assertNotEquals(invalido, obtido);
+    }
     
+    @Test
+    void testSetNumeroNulo(){
+        final Conta instance = new Conta();
+        assertThrows(NullPointerException.class, () -> instance.setNumero(null));
+    }
+    
+    @Test
+    void testInstanciaPadraoPoupanca(){
+        final Conta instance = new Conta();
+        assertFalse(instance.isPoupanca());
+    }
+    
+    @Test 
+    void testSetLimiteContaEspecial(){
+        final Conta instance = new Conta();
+        instance.setEspecial(true);
+        final double esperado = 1000;
+        instance.setLimite(esperado);
+        final double obtido = instance.getLimite();
+        assertEquals(esperado, obtido);
+    }
+    
+    @Test
+    void testSetLimiteContaNaoEspecial(){
+        final Conta instance = new Conta();
+        final double limite = 1000;
+        assertThrows(IllegalStateException.class, () -> instance.setLimite(limite));
+    }
+    
+    @Test
+    void testHistoricoNotNull(){
+        final Conta instance = new Conta();
+        assertNotNull(instance.getMovimentacoes());
+    }
+    
+    @Test
+    void testSetValorValido(){
+        final Conta instance = new Conta();
+        final Movimentacao movimentacao = new Movimentacao(instance);
+        final double esperado = 100;
+        movimentacao.setValor(esperado);
+        final double obtido = movimentacao.getValor();
+        assertEquals(esperado, obtido);
+    }
 }
